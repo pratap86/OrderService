9fa144011e79419c3d5a7357ee42aa4e5d296c78
pratap86 <narayanpratap86@gmail.com>
2021-07-13 11:24:29 +0530

    refactor shared module file name

--- src/main/java/com/pratap/estore/order/saga/OrderSaga.java
+++ src/main/java/com/pratap/estore/order/saga/OrderSaga.java
@@ -1,8 +1,11 @@
 package com.pratap.estore.order.saga;
 
-import com.pratap.estore.core.commands.ReserveProductCommand;
 import com.pratap.estore.order.core.events.OrderCreatedEvent;
+import com.pratap.estore.shared.commands.ProductReservedCommand;
+import com.pratap.estore.shared.events.ProductReservedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author Pratap Narayan
  */
 @Saga
+@Slf4j
 public class OrderSaga {
 
     @Autowired
@@ -21,17 +25,26 @@ public class OrderSaga {
     @SagaEventHandler(associationProperty = "orderId")
     private void handle(OrderCreatedEvent orderCreatedEvent){
 
-        ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+        ProductReservedCommand productReservedCommand = ProductReservedCommand.builder()
                 .orderId(orderCreatedEvent.getOrderId())
                 .productId(orderCreatedEvent.getProductId())
                 .quantity(orderCreatedEvent.getQuantity())
                 .userId(orderCreatedEvent.getUserId())
                 .build();
-        commandGateway.send(reserveProductCommand, ((commandMessage, commandResultMessage) -> {
+
+        log.info("OrderCreatedEvent handled for orderId={} and productId={}", productReservedCommand.getOrderId(), productReservedCommand.getProductId());
+        commandGateway.send(productReservedCommand, ((commandMessage, commandResultMessage) -> {
             if (commandResultMessage.isExceptional()){
                 //
             }
         }));
 
+    }
+
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(ProductReservedEvent productReservedEvent){
+
+        // process user payment here
+        log.info("ProductReservedEvent handled for orderId={} and productId={}", productReservedEvent.getOrderId(), productReservedEvent.getProductId());
     }
 }
