49f333d73c49a180b2de6e79c162e40585d8680b
pratap86 <narayanpratap86@gmail.com>
2021-07-14 20:28:29 +0530

    process user payment

--- src/main/java/com/pratap/estore/order/saga/OrderSaga.java
+++ src/main/java/com/pratap/estore/order/saga/OrderSaga.java
@@ -3,11 +3,14 @@ package com.pratap.estore.order.saga;
 import com.pratap.estore.order.core.events.OrderCreatedEvent;
 import com.pratap.estore.shared.commands.ProductReservedCommand;
 import com.pratap.estore.shared.events.ProductReservedEvent;
+import com.pratap.estore.shared.model.User;
+import com.pratap.estore.shared.query.FetchUserPaymentDetailsQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,6 +23,9 @@ public class OrderSaga {
 
     @Autowired
     private transient CommandGateway commandGateway;
+
+    @Autowired
+    private transient QueryGateway queryGateway;
 
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
@@ -43,8 +49,24 @@ public class OrderSaga {
 
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(ProductReservedEvent productReservedEvent){
-
-        // process user payment here
         log.info("ProductReservedEvent handled for orderId={} and productId={}", productReservedEvent.getOrderId(), productReservedEvent.getProductId());
+        // process user payment here
+        FetchUserPaymentDetailsQuery fetchUserPaymentDetailsQuery = new FetchUserPaymentDetailsQuery(productReservedEvent.getUserId());
+
+        User userPaymentDetails = null;
+
+        try {
+            userPaymentDetails = queryGateway.query(fetchUserPaymentDetailsQuery, ResponseTypes.instanceOf(User.class)).join();
+        } catch (Exception ex){
+            log.error(String.valueOf(ex));
+
+            // start compensating transaction
+        }
+
+        if (userPaymentDetails == null){
+            log.info("userPaymentDetails not Found by userId={}", productReservedEvent.getUserId());
+        } else {
+            log.info("successfully fetched userPaymentDetails by userId={} and firstName={}", productReservedEvent.getUserId(), userPaymentDetails.getFirstName());
+        }
     }
 }
